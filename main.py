49aedcaf91ebde1e3addb8a7a8cc72670a3ef4ab
Thisizasif <68771679+thisizasif@users.noreply.github.com>
2024-01-11 16:59:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 import itertools
+import os
 
-banner = """
- ███████████  ████                         ███████████                █████                  
-░░███░░░░░███░░███                        ░░███░░░░░███              ░░███                   
- ░███    ░███ ░███  █████ ████  ██████     ░███    ░███  ██████    ███████   ███████  ██████ 
- ░██████████  ░███  ░░███ ░███  ███░░███    ░██████████  ░░░░░███  ███░░███  ███░░███ ███░░███
- ░███░░░░░███ ░███  ░███ ░███ ░███████     ░███░░░░░███  ███████ ░███ ░███ ░███ ░███░███████ 
- ░███    ░███ ░███  ░███ ░███ ░███░░░      ░███    ░███ ███░░███ ░███ ░███ ░███ ░███░███░░░  
- ███████████  █████ ░░████████░░██████     ███████████ ░░████████░░████████░░███████░░██████ 
-░░░░░░░░░░░  ░░░░░   ░░░░░░░░  ░░░░░░     ░░░░░░░░░░░   ░░░░░░░░  ░░░░░░░░  ░░░░░███ ░░░░░░  
-                                                                            ███ ░███         
-                                                                           ░░██████          
-                                                                            ░░░░░░           
-"""
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-print(banner)
+def generate_permutations(input_str):
+    perms = list(itertools.permutations(input_str))
+    total_permutations = len(perms)
 
-input_str = input("Enter the input: ")
+    print(f"\033[1;32mGenerating permutations... (Total: {total_permutations})\033[0m")
 
-perms = list(itertools.permutations(input_str))
-total_permutations = len(perms)
+    with open('BlueBadge.txt', 'w') as file:
+        for i, perm in enumerate(perms, start=1):
+            password = ''.join(perm)
+            file.write(password + '\n')
+            print(f"\033[1;36mGenerated password {i}/{total_permutations}: {password}\033[0m", end='\r')
 
-print(f"\nGenerating permutations... (Total: {total_permutations})\n")
+    print(f"\n\033[1;32mPermutations saved to BlueBadge.txt.\033[0m")
 
-with open('BlueBadge.txt', 'w') as file:
-    for i, perm in enumerate(perms, start=1):
-        password = ''.join(perm)
-        file.write(password + '\n')
-        print(f"Generated password {i}/{total_permutations}: {password}")
+def main():
+    clear_terminal()
+  print("\033[1;33m###############################################\033[0m")
+  print("\033[1;33m#                                             #\033[0m")
+  print("\033[1;33m#        [ Blue Badge ]              #\033[0m")
+  print("\033[1;33m#               Version: 1.0.0               #\033[0m")
+  print("\033[1;33m#                 Author: Thisizasif          #\033[0m")
+  print("\033[1;33m#                                             #\033[0m")
+  print("\033[1;33m###############################################\033[0m")
+  print("\n")
 
-print("\nPermutations saved to BlueBadge.txt.")
+    
+
+    input_str = input("\033[1;32mEnter the input: \033[0m")
+    generate_permutations(input_str)
+
+    input("\033[1;36mPress Enter to continue...\033[0m")
+    clear_terminal()
+
+if __name__ == "__main__":
+    main()
