cf56eea66fa20700d961adc8257d7541bb1f0ee3
Thisizasif <68771679+thisizasif@users.noreply.github.com>
2024-01-12 16:10:40 +0530

    Update unlockpdf.py

--- unlockpdf.py
+++ unlockpdf.py
@@ -1,21 +1,15 @@
 import pikepdf
 from tqdm import tqdm
 
-banner = """
-
-"""
-
-print(banner)
-
 # load password list
-passwords = [line.strip() for line in open("BlueBadge.txt")]
+passwords = [ line.strip() for line in open("BlueBadge.txt") ]
 
 # iterate over passwords
 for password in tqdm(passwords, "Decrypting PDF"):
     try:
         # open PDF file
         with pikepdf.open("bluebadge.pdf", password=password) as pdf:
-            # Password decrypted successfully, print immediately
+            # Password decrypted successfully, break out of the loop
             print("[+] Password found:", password)
             break
     except pikepdf._core.PasswordError as e:
