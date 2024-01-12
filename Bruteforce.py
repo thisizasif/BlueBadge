7152d654e1dd4154aa12111a5b877d712b0a6e0f
Thisizasif <68771679+thisizasif@users.noreply.github.com>
2024-01-12 09:57:51 +0530

    Update Bruteforce.py

--- Bruteforce.py
+++ Bruteforce.py
@@ -1,33 +1,24 @@
-import pikepdf
+import fitz  # PyMuPDF
 from tqdm import tqdm
 
 banner = """
- ███████████  ████                         ███████████                █████                  
-░░███░░░░░███░░███                        ░░███░░░░░███              ░░███                   
- ░███    ░███ ░███  █████ ████  ██████     ░███    ░███  ██████    ███████   ███████  ██████ 
- ░██████████  ░███  ░░███ ░███  ███░░███    ░██████████  ░░░░░███  ███░░███  ███░░███ ███░░███
- ░███░░░░░███ ░███  ░███ ░███ ░███████     ░███░░░░░███  ███████ ░███ ░███ ░███ ░███░███████ 
- ░███    ░███ ░███  ░███ ░███ ░███░░░      ░███    ░███ ███░░███ ░███ ░███ ░███ ░███░███░░░  
- ███████████  █████ ░░████████░░██████     ███████████ ░░████████░░████████░░███████░░██████ 
-░░░░░░░░░░░  ░░░░░   ░░░░░░░░  ░░░░░░     ░░░░░░░░░░░   ░░░░░░░░  ░░░░░░░░  ░░░░░███ ░░░░░░  
-                                                                            ███ ░███         
-                                                                           ░░██████          
-                                                                            ░░░░░░           
+       
 """
 
 print(banner)
 
 # load password list
-passwords = [ line.strip() for line in open("BlueBadge.txt") ]
+passwords = [line.strip() for line in open("BlueBadge.txt")]
 
 # iterate over passwords
 for password in tqdm(passwords, "Decrypting PDF"):
     try:
         # open PDF file
-        with pikepdf.open("pdf/Aaaa.pdf", password=password) as pdf:
+        with fitz.open("pdf/Aaaa.pdf", password=password) as pdf:
             # Password decrypted successfully, break out of the loop
             print("[+] Password found:", password)
             break
-    except pikepdf._core.PasswordError as e:
-        # wrong password, just continue in the loop
+    except Exception as e:
+        # wrong password or other exception, just continue in the loop
         continue
+
